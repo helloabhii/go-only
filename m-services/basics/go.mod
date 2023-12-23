0de58087440e0089ce74c478935c9abb821f4d68
Abhishek Verma <ak8888780@gmail.com>
2023-12-23 21:49:09 +0530

    fundamentals

---
+++ m-services/basics/go.mod
@@ -0,0 +1,7 @@
+module github.com/helloabhii/go-only/m-services
+
+go 1.21.3
+
+require github.com/gorilla/handlers v1.5.2
+
+require github.com/felixge/httpsnoop v1.0.3 // indirect
--- m-services/basics/main.go
+++ m-services/basics/main.go
@@ -1,26 +1,49 @@
 package main
 
 import (
-	"io"
+	"context"
 	"log"
 	"net/http"
-  "fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/helloabhii/go-only/m-services/handlers"
 )
 
 func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Helloabhii")
-		data, err := io.ReadAll(r.Body)
-    if err != nil {
-      http.Error(rw, "Oops", http.StatusBadRequest)
-      return
-    }
-		fmt.Fprintf(rw, "Hello %s", data)
-	})
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbyeabhii")
-	})
-	http.ListenAndServe(":9090", nil)
+	//creating new serve mux
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm, //serve mux
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	go func() { //handling listen and serve  go func so it's not gonna block
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal) //signal broadcast a message on sigchan when  interrupting/ kill  happens
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc) //shutdown takes the context
+
+	// http.ListenAndServe(":9090", sm) //listen and serve takes 2 parameters one is address 2nd ishttp handler
 }
