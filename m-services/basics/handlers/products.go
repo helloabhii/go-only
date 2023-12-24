d4c3aa0821d7dafba06e7c3269af4d71abbe6841
Abhishek Verma <ak8888780@gmail.com>
2023-12-24 10:38:34 +0530

    REST startting

--- m-services/basics/handlers/products.go
+++ m-services/basics/handlers/products.go
@@ -21,15 +21,27 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// Handle update
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
 
 	//catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handler Get Product")
+
 	lp := data.GetProducts()
+
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handler Post Product")
+	//taking data from post and convert into product object
 }
