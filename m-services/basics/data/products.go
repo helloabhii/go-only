28677802eda9f6af8c63039a17eb2315903e4db4
Abhishek Verma <ak8888780@gmail.com>
2023-12-24 07:37:10 +0530

    microservices fundamentals

---
+++ m-services/basics/data/products.go
@@ -0,0 +1,50 @@
+package data
+
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
+
+type Product struct {
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"` //ommiting this
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
+}
+
+type Products []*Product
+
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w) //encoder
+	return e.Encode(p)
+}
+
+func GetProducts() Products {
+	return productList
+}
+
+var productList = Products{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc321",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong without milk",
+		Price:       7.1,
+		SKU:         "a71",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
