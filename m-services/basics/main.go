f7634fbc640783291fac17de48f06de7ef562cbb
Abhishek Verma <ak8888780@gmail.com>
2023-12-29 11:48:23 +0530

    cross-origin-resources-sharing

--- m-services/basics/main.go
+++ m-services/basics/main.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	"github.com/go-openapi/runtime/middleware"
-
+	gohandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/helloabhii/go-only/m-services/data"
 	"github.com/helloabhii/go-only/m-services/handlers"
@@ -54,10 +54,13 @@ func main() {
 	getR.Handle("/docs", sh)
 	getR.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
 
+	// CORS
+	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"*"}))
+
 	// create a new server
 	s := http.Server{
 		Addr:         *bindAddress,      // configure the bind address
-		Handler:      sm,                // set the default handler
+		Handler:      ch(sm),            // set the default handler
 		ErrorLog:     l,                 // set the logger for the server
 		ReadTimeout:  5 * time.Second,   // max time to read request from the client
 		WriteTimeout: 10 * time.Second,  // max time to write response to the client
