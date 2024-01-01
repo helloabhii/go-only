00448e7c52a355d01061dba08ab9687e43bdb40a
Abhishek Verma <ak8888780@gmail.com>
2024-01-01 22:52:47 +0530

    basics

--- deep-dive/01/http-get/helloworld/main.go
+++ deep-dive/01/http-get/helloworld/main.go
@@ -15,8 +15,9 @@ func main() {
 }
 
 func argu() {
-	args := os.Args //arguments
-	fmt.Printf("helloabhii \n Arguments :  %v\n", args)
+	args := os.Args                                                                   //arguments
+	fmt.Printf("helloabhii \n os  Arguments :  %v\n Argument: %v\n", args, args[1:3]) // you  have to give the arguments otherwise cause an error
 	// %v means compiler auto decide what the type of that agrument
 	//printf because of formatting
+	// go run main.go arg1 arg2
 }
